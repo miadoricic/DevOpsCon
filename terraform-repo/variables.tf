a248f81397052e3aa3487aa7e5aaba5104fcf299
Mia Doričić <miadoricic@gmail.com>
2026-04-21 10:14:22 +0000

    adding variable validation

--- terraform-repo/variables.tf
+++ terraform-repo/variables.tf
@@ -7,7 +7,14 @@ variable "repository_description" {
     description = "A description for the DevOpsCon wTerraform workshop"
     type        = string
     default     = "An example repository created with Terraform"
+
+    validation {
+      condition     = length(var.repository_description) > 0
+      error_message = "The repository description cannot be empty."
+    }
 }
+
+
 
 variable "repository_private" {
     description = "Whether the repository should be private"
