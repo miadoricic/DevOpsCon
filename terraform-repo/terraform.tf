107798b48e2d13fdebf2ef346cfdd7c051e4fd75
Mia Doričić <miadoricic@gmail.com>
2026-04-21 11:26:38 +0000

    remove comment

--- terraform-repo/terraform.tf
+++ terraform-repo/terraform.tf
@@ -4,7 +4,7 @@ terraform{
     required_providers {
         github = {
             source  = "integrations/github"
-            version = "-> 6.11" #goes to 6.99 but not 7.0+, if set 6.11.1 then goes up to 6.11.9, not 6.12.1+
+            version = "-> 6.11" 
         }
     }
 }
